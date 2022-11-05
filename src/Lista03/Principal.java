4c6345fa86d5e259cfaaabed2f01bee265b0daf5
EmilianoCostaJr <emiliano.costajr@gmail.com>
2022-11-05 10:34:38 -0300

    Update Principal.java
    Modificação do método drive()
    Criação do método seleciona()

--- src/Lista03/Principal.java
+++ src/Lista03/Principal.java
@@ -12,28 +12,66 @@
 
 package Lista03;
 
+import java.util.Scanner;
+
 /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 public class Principal {
 /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
     /**Procedimento principal que inicia e termina a aplicação*/
     public static void main(String[] args){
-        _Exer_03_11.AccountTest.inicio();
+        seleciona();
     } /*FIM Método main()*/
 
     /**Procedimento drive: Piloto que conduz a execução do aplicativo*/
-    public static void drive(){
-        _Exer_03_11.AccountTest.inicio();
-//        _Exer_03_11.AccountTest.inicioD();/*Versão caixa de dialogo*/
-        _Exer_03_12.InvoiceTest.inicio();
-//        _Exer_03_12.InvoiceTest.inicioD();/*Versão caixa de dialogo*/
-        _Exer_03_13.EmpregadoTest.inicio();/*Exercicio 13*/
-        _Exer_03_14.DateTest.inicio();/*Exercicio 14*/
-        _Exer_03_15.AccountTest.inicio();
-//        _Exer_03_15.AccountTest.inicioD();
-        _Exer_03_16.Calculadora_Cardiaca.inicio();/*Exercicio 16*/
-        _Exer_03_17.HealthProfileTest.inicio();
+    public static void drive(int escolha){
+        switch (escolha){
+            case 11:        
+                _Exer_03_11.AccountTest.inicio();
+        //        _Exer_03_11.AccountTest.inicioD();/*Versão caixa de dialogo*/
+                break;
+            case 12:
+                _Exer_03_12.InvoiceTest.inicio();
+        //        _Exer_03_12.InvoiceTest.inicioD();/*Versão caixa de dialogo*/
+                break;
+            case 13:
+                _Exer_03_13.EmpregadoTest.inicio();/*Exercicio 13*/
+                break;
+            case 14:
+                _Exer_03_14.DateTest.inicio();/*Exercicio 14*/
+                break;
+            case 15:
+                _Exer_03_15.AccountTest.inicio();
+                //        _Exer_03_15.AccountTest.inicioD();
+                break;
+            case 16:
+                _Exer_03_16.Calculadora_Cardiaca.inicio();/*Exercicio 16*/
+                break;
+            case 17:
+                _Exer_03_17.HealthProfileTest.inicio();
+                break;
+            default: 
+                System.out.println("Nenhum exercício escolhido, escolha novamente: ");
+            }
+            seleciona();                
     }/*FIM Método drive()*/
+
+    public static void seleciona(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n\nMenu de Escolha.\n");
+        System.out.println("Digite zero para sair.");
+        System.out.print("Ou digite o número do exercício a ser executado: ");
+
+        int selecionado = 0;
+        try{
+            selecionado = input.nextInt(); 
+        }catch(Exception e){
+            selecionado = 0; 
+        }
+
+        if(selecionado != 0)
+            drive(selecionado);        
+    }
 
 
 }/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::Fim da classe Principal*/
